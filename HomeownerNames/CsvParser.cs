1586c659075ea1b2b8cef589d1ac5f961be3c8f8
devagx <alungroome@hotmail.com>
2021-04-23 19:13:01 +0100

    Created error handling in CsvParser constructor for ArgumentNullException

--- HomeownerNames/CsvParser.cs
+++ HomeownerNames/CsvParser.cs
@@ -1,10 +1,15 @@
-﻿namespace HomeownerNames
+﻿using System;
+
+namespace HomeownerNames
 {
     public class CsvParser
     {
         public CsvParser(string filePath)
         {
-
+            if (filePath == null)
+            {
+                throw new ArgumentNullException("filePath");
+            }
         }
     }
 }

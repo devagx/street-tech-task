051c977539e2b78f6424a1eae0acf2c8dfaa8a02
devagx <alungroome@hotmail.com>
2021-04-23 21:33:02 +0100

    Implemented Solution class to allow a user to run the programme from command line and display results to console.

--- HomeownerNames/Solution.cs
+++ HomeownerNames/Solution.cs
@@ -1,10 +1,24 @@
-﻿namespace HomeownerNames
+﻿using System;
+
+namespace HomeownerNames
 {
     public class Solution
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Starting CSV Parser Application...");
+            Console.WriteLine("Please enter full path to CSV file:");
+            string filePath = Console.ReadLine();
+            CsvParser parser = new CsvParser(filePath);
+            parser.ProcessAllLines();
 
+            foreach (var person in parser.People)
+            {
+                Console.WriteLine("Title = " + person.Title);
+                Console.WriteLine("FirstName = " + person.FirstName);
+                Console.WriteLine("Initial = " + person.Initial);
+                Console.WriteLine("LastName = " + person.LastName);
+            }
         }
     }
 }

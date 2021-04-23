69c81417f0d513ecb99b6af57afaf981bdf3136e
devagx <alungroome@hotmail.com>
2021-04-23 19:24:12 +0100

    Created People property to store a list of individual persons.

--- HomeownerNames.Tests/CsvParserTests.cs
+++ HomeownerNames.Tests/CsvParserTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace HomeownerNames.Tests
--- HomeownerNames/CsvParser.cs
+++ HomeownerNames/CsvParser.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HomeownerNames
 {
     public class CsvParser
     {
+        private List<Person> people = new List<Person>();
+
+        public List<Person> People
+        {
+            get { return people; }
+        }
         public CsvParser(string filePath)
         {
             if (filePath == null)

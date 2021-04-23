1020bbc9ad3ec64929a971c35a44fab84ab72ba0
devagx <alungroome@hotmail.com>
2021-04-23 20:14:45 +0100

    implemented PersonIdentifier in CsvParser

--- HomeownerNames/CsvParser.cs
+++ HomeownerNames/CsvParser.cs
@@ -28,7 +28,81 @@ namespace HomeownerNames
         }
         public void ProcessSingleLine(string line)
         {
+            PersonIdentifier(line);
+        }
+        private void PersonIdentifier(string line)
+        {
+            string[] parts = line.Split(' ');
 
+            if (parts.Length == 4)
+            {
+                Person person = new Person
+                {
+                    Title = parts[0],
+                    LastName = parts[parts.Length - 1].Substring(0, parts[parts.Length - 1].Length - 1)
+                };
+                people.Add(person);
+                person = new Person
+                {
+                    Title = parts[2],
+                    LastName = parts[parts.Length - 1].Substring(0, parts[parts.Length - 1].Length - 1)
+                };
+                people.Add(person);
+            }
+            else if (parts.Length == 5)
+            {
+                Person person = new Person
+                {
+                    Title = parts[0],
+                    FirstName = parts[3],
+                    LastName = parts[parts.Length - 1].Substring(0, parts[parts.Length - 1].Length - 1)
+                };
+                people.Add(person);
+                person = new Person
+                {
+                    Title = parts[2],
+                    FirstName = parts[3],
+                    LastName = parts[parts.Length - 1].Substring(0, parts[parts.Length - 1].Length - 1)
+                };
+                people.Add(person);
+            }
+            else if (parts.Length == 7)
+            {
+                Person person = new Person
+                {
+                    Title = parts[0],
+                    FirstName = parts[1],
+                    LastName = parts[2]
+                };
+                people.Add(person);
+                person = new Person
+                {
+                    Title = parts[4],
+                    FirstName = parts[5],
+                    LastName = parts[6].Substring(0, parts[6].Length - 1)
+                };
+                people.Add(person);
+            }
+            else if (parts.Length == 3 && parts[1].Length > 2)
+            {
+                Person person = new Person
+                {
+                    Title = parts[0],
+                    FirstName = parts[1],
+                    LastName = parts[parts.Length - 1].Substring(0, parts[parts.Length - 1].Length - 1)
+                };
+                people.Add(person);
+            }
+            else if (parts.Length == 3 && parts[1].Length <= 2)
+            {
+                Person person = new Person
+                {
+                    Title = parts[0],
+                    Initial = parts[1].Substring(0, 1),
+                    LastName = parts[parts.Length - 1].Substring(0, parts[parts.Length - 1].Length - 1)
+                };
+                people.Add(person);
+            }
         }
     }
 }
